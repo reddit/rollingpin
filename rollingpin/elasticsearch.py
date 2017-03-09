3913a14cfadbc0e48bb6b743d01b7577e3c17bec
Dan <ellisd23@gmail.com>
2017-03-09 11:13:43 -0800

    Update deploy annotation with commit ID.

--- rollingpin/elasticsearch.py
+++ rollingpin/elasticsearch.py
@@ -6,7 +6,7 @@ import time
 
 from twisted.internet import reactor
 from twisted.internet.defer import inlineCallbacks, succeed
-from twisted.web.client import Agent
+from twisted.web.client import Agent, readBody
 from twisted.web.http_headers import Headers
 from twisted.web.iweb import IBodyProducer
 from zope.interface import implements
@@ -62,6 +62,42 @@ class ElasticSearchNotifier(object):
                 body,
             )
 
+    def update_doc(self, id_, updated_fields):
+        """ Update a document in Elasticsearch
+        :param updated_fields: dictionary values to update
+        :return Deferred
+        """
+        # The twisted web client chokes on unicode URLs, so we'll coerce this
+        # back to a string
+        update_endpoint = str("%s/%s/_update" % (self.endpoint, id_))
+
+        # Unlike indexing, for updates we have to put our updated fields inside
+        # a "doc" key.
+        #
+        # https://www.elastic.co/guide/en/elasticsearch/guide/current/partial-updates.html
+        doc = {'doc': updated_fields}
+
+        with swallow_exceptions("elasticsearch", self.logger):
+            agent = Agent(reactor)
+            body = JSONBodyProducer(json.dumps(doc))
+            return agent.request(
+                'POST',
+                update_endpoint,
+                Headers({'User-Agent': ['rollingpin']}),
+                body,
+            )
+
+    def build_sync_doc(self, sync_info):
+        # We aren't aware of the commit ID that is being deployed at initial
+        # deploy time.  Once the build sync has happened, we have this
+        # information, so we go back and update the original document with a
+        # list of the sync targets (i.e. commit IDs being deployed).
+        sync = ["%s@%s" % (k, v.get('token', '')[:7])
+                for k, v in sync_info.iteritems()]
+        return {
+            'sync_targets': ', '.join(sync),
+        }
+
     def deploy_start_doc(self):
         timestamp_in_milliseconds = int(time.time()) * 1000
         return {
@@ -95,8 +131,20 @@ class ElasticSearchNotifier(object):
         }
 
     @inlineCallbacks
+    def on_build_sync(self, sync_info):
+        yield self.update_doc(self.deploy_annotation_id,
+                              self.build_sync_doc(sync_info))
+
+    @inlineCallbacks
     def on_deploy_start(self):
-        yield self.index_doc(self.deploy_start_doc())
+        # Store the initial document's ID so we can update it with additional
+        # metadata later in the deploy process.
+        response = yield self.index_doc(self.deploy_start_doc())
+        body = yield readBody(response)
+        if response.code != 201:
+            self.logger.error('Could not store deploy metadata.  '
+                              'Got response %s', body)
+        self.deploy_annotation_id = json.loads(body).get('_id', '')
 
     @inlineCallbacks
     def on_deploy_abort(self, reason):
@@ -112,6 +160,7 @@ def enable_elastic_search_notifications(config, event_bus, components, hosts,
     notifier = ElasticSearchNotifier(
         config, components, hosts, command_line, word)
     event_bus.register({
+        "build.sync": notifier.on_build_sync,
         "deploy.begin": notifier.on_deploy_start,
         "deploy.abort": notifier.on_deploy_abort,
         "deploy.end": notifier.on_deploy_end,
