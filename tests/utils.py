5c1906a4ba3d3a9ac7ffcd4a6235fb3d31208731
Saurabh Sharma <saurabh.sharma@reddit.com>
2017-03-24 14:40:48 -0700

    CI script properly bubbles failure, finds all py files

--- tests/utils.py
+++ tests/utils.py
@@ -7,6 +7,7 @@ from rollingpin.utils import swallow_exceptions
 
 
 class TestUtils(unittest.TestCase):
+
     def test_swallow_exception_on_error(self):
         logger = Mock()
         exception = Exception("fail")
