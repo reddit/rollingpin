22cbb4bd12e66afb99d8eefe11636b64e52a79ae
Neil Williams <neil@reddit.com>
2017-12-15 16:04:34 -0800

    Generate pools in the mock hostsource
    Just based on hostname prefix. This allows for easier testing of
    pool-aware stuff.

--- rollingpin/hostsources/mock.py
+++ rollingpin/hostsources/mock.py
@@ -17,7 +17,7 @@ class MockHostSource(HostSource):
         self.hosts = config["hostsource"]["hosts"].split()
 
     def get_hosts(self):
-        return succeed(Host(name, name, name, "")
+        return succeed(Host(name, name, name, name.split("-")[0])
                        for name in self.hosts)
 
     def should_be_alive(self, host):
